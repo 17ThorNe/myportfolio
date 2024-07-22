2416e810c78e82b043a5c5af18488f9bf08b4bfb
17ThorNe <1220.sem.thorne.rupp.edu.kh@gmail.com>
2024-07-22 11:46:52 +0700

    hello

---
+++ src/component/React/Home.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./Home.css";
+
+function HomePage() {
+  const [text, setText] = useState("");
+  function btnDownloadMyCV() {
+    setText("Prank Test");
+  }
+
+  return (
+    <section>
+      <div className="contain_nav_bar">
+        <p>THORNE</p>
+        <ul>
+          <li>
+            <a href="#">About me</a>
+          </li>
+          <li>
+            <a href="#">Skill</a>
+          </li>
+          <li>
+            <a href="#">Experience</a>
+          </li>
+          <li>
+            <a href="#">Contac me</a>
+          </li>
+        </ul>
+        <button onClick={btnDownloadMyCV}>Download CV</button>
+      </div>
+      <h2 className="showTextHere">{text}</h2>
+    </section>
+  );
+}
+
+export default HomePage;
